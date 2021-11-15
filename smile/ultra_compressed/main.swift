591923f014a05806e487df35e570dfac9ceb5b60
Pedro Paulo de Amorim <pp.amorim@hotmail.com>
2021-11-15 01:28:45 +0000

    Add code to generate the header for the files

---
+++ smile/ultra_compressed/main.swift
@@ -0,0 +1,32 @@
+import Foundation
+ 
+var lines: [String] = []
+let localPathString = "~/Repository/M38V1-A2/smile/ultra_compressed_copy/"
+
+for i in 0...41 {
+    let location = NSString(string: "\(localPathString)\(i).jpg").expandingTildeInPath    
+    let data: Data = NSData(contentsOfFile: location)! as Data
+    lines.append("const uint8_t smile_\(i)[] PROGMEM = {")
+    lines.append(hexTo0XPrefix(data.hexEncodedString()).joined(separator: ", "))
+    lines.append("};")
+}
+
+let source = lines.joined(separator: "\n")
+
+do {
+  try source.write(toFile: NSString(string: "\(localPathString)output.h").expandingTildeInPath, atomically: true, encoding: String.Encoding.utf8)
+} catch {
+  print(error)
+}
+
+//print(source)
+
+func hexTo0XPrefix(_ bin: [String]) -> [String] {
+  bin.map { bin in "0x\(bin.uppercased())" }
+}
+
+extension Data {
+  func hexEncodedString() -> [String] {
+    map { String(format: "%02hhx", $0) }
+  }
+}
